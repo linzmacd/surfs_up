f8b184607d2c4d2b38ac03117d0197afac9bb3e4
Lindsay MacDonald <linzmacd@gmail.com>
2021-03-26 22:50:40 -0700

    Updating app.py file

--- app.py
+++ app.py
@@ -42,30 +42,37 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    session = Session(engine)
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
+    session.close()
     return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
 def stations():
+    session = Session(engine)
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
+    session.close()
     return jsonify(stations=stations)
 
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
+    session = Session(engine)
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     results = session.query(Measurement.tobs).\
       filter(Measurement.station == 'USC00519281').\
       filter(Measurement.date >= prev_year).all()
     temps = list(np.ravel(results))
+    session.close()
     return jsonify(temps=temps)
 
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
+    session = Session(engine)
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
     if not end:
@@ -79,4 +86,10 @@ def stats(start=None, end=None):
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
+    session.close()
     return jsonify(temps=temps)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    # session.close() in each app route
